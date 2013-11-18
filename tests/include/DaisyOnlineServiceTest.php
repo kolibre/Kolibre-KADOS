6d560964a35c35c43db824db7744936a74f962b7
Johan Abbors <johan@pratsam.org>
2013-11-18 16:14:24 +0200

    Add skeleton for TestAdapter and DaisyOnlineService unit test

---
+++ tests/include/DaisyOnlineServiceTest.php
@@ -0,0 +1,163 @@
+<?php
+
+/*
+ * Copyright (C) 2013 Kolibre
+ *
+ * This file is part of Kolibre-KADOS.
+ * Kolibre-KADOS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * at your option) any later version.
+ *
+ * Kolibre-KADOS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Kolibre-KADOS. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+$includePath = dirname(realpath(__FILE__)) . '/../../include';
+set_include_path(get_include_path() . PATH_SEPARATOR . $includePath);
+
+require_once('DaisyOnlineService.class.php');
+
+class DaisyOnlineServiceTest extends PHPUnit_Framework_TestCase
+{
+    protected static $inifile;
+    protected static $instance;
+
+    public static function setUpBeforeClass()
+    {
+        self::$inifile = realpath(dirname(__FILE__)) . '/service.ini';
+        if (file_exists(self::$inifile)) unlink(self::$inifile);
+
+        $settings = array();
+        $settings['Service'] = array();
+        $settings['Adapter'] = array();
+        $settings['Adapter']['name'] = 'TestAdapter';
+        $settings['Adapter']['path'] = realpath(dirname(__FILE__));
+
+        self::write_ini_file($settings, self::$inifile);
+
+        self::$instance = new DaisyOnlineService(self::$inifile);
+        self::$instance->disableInternalSessionHandling();
+    }
+
+    public static function tearDownAfterClass()
+    {
+        if (file_exists(self::$inifile)) unlink(self::$inifile);
+    }
+
+    public static function write_ini_file($settings, $file)
+    {
+        $content = '';
+        foreach ($settings as $group => $value)
+        {
+            $content .= "[$group]\n";
+            foreach ($value as $key => $value)
+            {
+                if (is_array($value))
+                {
+                    foreach ($value as $key2 => $value)
+                        $content .= "$key" . "[] = $value\n";
+                }
+                else
+                    $content .= "$key = $value\n";
+            }
+        }
+
+        if (!$handle = fopen($file, 'w'))
+            return false;
+
+        if (!fwrite($handle, $content))
+            return false;
+
+        fclose($handle);
+        return true;
+    }
+
+    /**
+     * @group daisyonlineservice
+     * @group operation
+     */
+    public function testLogOn()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @group daisyonlineservice
+     * @group operation
+     */
+    public function testLogOff()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @group daisyonlineservice
+     * @group operation
+     */
+    public function testGetServiceAttributes()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @group daisyonlineservice
+     * @group operation
+     */
+    public function testSetReadingSystemAttributes()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @group daisyonlineservice
+     * @group operation
+     */
+    public function testGetContentList()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @group daisyonlineservice
+     * @group operation
+     */
+    public function testGetContentMetadata()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @group daisyonlineservice
+     * @group operation
+     */
+    public function testIssueContent()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @group daisyonlineservice
+     * @group operation
+     */
+    public function testGetContentResouces()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @group daisyonlineservice
+     * @group operation
+     */
+    public function testReturnContent()
+    {
+        $this->assertTrue(true);
+    }
+}
+
+?>
---
+++ tests/include/TestAdapter.class.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+ * Copyright (C) 2013 Kolibre
+ *
+ * This file is part of Kolibre-KADOS.
+ * Kolibre-KADOS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * at your option) any later version.
+ *
+ * Kolibre-KADOS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Kolibre-KADOS. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+$includePath = dirname(realpath(__FILE__)) . '/../../include/adapter';
+set_include_path(get_include_path() . PATH_SEPARATOR . $includePath);
+
+require_once('Adapter.class.php');
+
+class TestAdapter extends Adapter
+{
+    public function label($id, $type, $language = null)
+    {
+    }
+
+    public function authenticate($username, $password)
+    {
+    }
+
+    public function contentListExists($list)
+    {
+    }
+
+    public function contentList($list, $contentFormats = null, $protectionFormats = null, $mimeTypes = null)
+    {
+    }
+
+    public function contentExists($contentId)
+    {
+    }
+
+    public function contentAccessible($contentId)
+    {
+    }
+
+    public function contentReturnDate($contentId)
+    {
+    }
+
+    public function contentMetadata($contentId)
+    {
+    }
+
+    public function contentIssuable($contentId)
+    {
+    }
+
+    public function contentIssue($contentId)
+    {
+    }
+
+    public function contentResources($contentId)
+    {
+    }
+
+    public function contentReturnable($contentId)
+    {
+    }
+
+    public function contentReturn($contentId)
+    {
+    }
+}
+
+?>

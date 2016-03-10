55ae4b6267c88dfd3d4ec6790e38e6cad0da7149
Fredrik <fabbors@gmail.com>
2016-03-10 18:09:55 +0200

    implemented bookmarkObject class and unit tests

---
+++ include/types/bookmarkObject.class.php
@@ -0,0 +1,129 @@
+<?php
+
+/*
+ * Copyright (C) 2013 Kolibre
+ *
+ * This file is part of Kolibre-KADOS.
+ * Kolibre-KADOS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * at your option) any later version.
+ *
+ * Kolibre-KADOS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Kolibre-KADOS. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once('AbstractType.class.php');
+require_once('bookmarkSet.class.php');
+
+
+class bookmarkObject extends AbstractType {
+
+   
+
+    /**
+     * @var (object)bookmarkSet
+     */
+    public $bookmarkSet;
+
+     /**
+     * @var string with format YYYY-MM-DDThh:mm:ss+hh:mm or YYYY-MM-DDThh:mm:ssZ
+     */
+    public $lastModifiedDate;
+
+    /******************** public functions ********************/
+
+    /**
+     * constructor for class bookmarkObject
+     */
+    function __construct($_bookmarkSet = NULL,$_lastModifiedDate = NULL) {
+        if (is_string($_bookmarkSet)) $this->setBookmarkSet($_bookmarkSet);
+        if (is_string($_lastModifiedDate)) $this->setLastModifiedDate($_lastModifiedDate);
+        
+    }
+
+
+    /******************** class get set methods ********************/
+
+    /**
+     * getter for lastModifiedDate
+     */
+    function getLastModifiedDate() {
+        return $this->lastModifiedDate;
+    }
+
+    /**
+     * setter for lastModifiedDate
+     */
+    function setLastModifiedDate($_lastModifiedDate) {
+        $this->lastModifiedDate = $_lastModifiedDate;
+    }
+
+    /**
+     * resetter for lastModifiedDate
+     */
+    function resetLastModifiedDate() {
+        $this->lastModifiedDate = NULL;
+    }
+
+    /**
+     * getter for bookmarkSet
+     */
+    function getBookmarkSet() {
+        return $this->bookmarkSet;
+    }
+
+    /**
+     * setter for bookmarkSet
+     */
+    function setBookmarkSet($_bookmarkSet) {
+        $this->bookmarkSet = $_bookmarkSet;
+    }
+
+    /**
+     * resetter for bookmarkSet
+     */
+    function resetBookmarkSet() {
+        $this->bookmarkSet = NULL;
+    }
+
+
+    /******************** validator methods ********************/
+
+    /**
+     * validator for class bookmarkObject
+     */
+    function validate() {
+                  
+        //bookmarkSet must be string of type bookmarkSet and correct
+        if (!is_null($this->bookmarkSet)) {
+            if ($this->isInstanceOf($this->bookmarkSet, 'bookmarkSet') === false){
+                return false;
+            }
+            if ($this->bookmarkSet->validate() === false) {
+                $this->error = __CLASS__ . '.' . $this->bookmarkSet->getError();
+                return false;
+            }
+        }
+
+        //lastModifiedDate must occur exactly once
+        if (!is_null($this->lastModifiedDate)){
+            if($this->isString($this->lastModifiedDate, 'lastModifiedDate') === false){
+                return false;
+            }
+        } 
+        if (preg_match('/\d{4}\-\d{2}\-\d{2}T\d{2}:\d{2}:\d{2}(\+\d{2}:\d{2}|Z)/', $this->lastModifiedDate) != 1) 
+            return false;    
+        
+            
+
+        return true;
+    }
+}
+
+?>
---
+++ tests/include/types/bookmarkObjectTest.php
@@ -0,0 +1,92 @@
+<?php
+
+/*
+ * Copyright (C) 2013 Kolibre
+ *
+ * This file is part of Kolibre-KADOS.
+ * Kolibre-KADOS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * at your option) any later version.
+ *
+ * Kolibre-KADOS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Kolibre-KADOS. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+$includePath = dirname(realpath(__FILE__)) . '/../../../include/types';
+set_include_path(get_include_path() . PATH_SEPARATOR . $includePath);
+
+require_once('bookmarkObject.class.php');
+require_once('bookmarkSet.class.php');
+require_once('title.class.php');
+require_once('lastmark.class.php');
+require_once('bookmarkAudio.class.php');
+
+class bookmarkObjectTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @group bookmarkObject
+     * @group validate
+     */
+
+    public function testBookmarkSet()
+    {
+        
+        $bookmarkObject = new bookmarkObject('test', '2016-03-11T14:23:23+00:00');
+        $this->assertFalse($bookmarkObject->validate());
+        //$this->assertContains('bookmarkObject.bookmarkSet', $bookmarkObject->getError());
+
+        $bookmarkAudio = new bookmarkAudio('src','clipBegin','ClipEnd');
+        $title = new title('title',$bookmarkAudio);
+        $lastmark = new lastMark('uri','uri','time', 1234);
+        
+        $bookmarkSet = new bookmarkSet($title, 'uid', $lastmark);
+        // check to see thast the bookmarkset is a valide object
+        $this->assertTrue($bookmarkSet->validate());
+
+        
+        $bookmarkObject = new bookmarkObject($bookmarkSet, '2016-03-11T14:23:23+00:00');
+        $this->assertTrue($bookmarkObject->validate());
+        //$this->assertContains('bookmarkObject.bookmarkSet', $bookmarkObject->getError());
+    }
+
+
+    /**
+     * @group bookmarkObject
+     * @group validate
+     */
+
+    public function testLastDateModified()
+    {
+        $bookmarkAudio = new bookmarkAudio('src','clipBegin','ClipEnd');
+        $title = new title('title',$bookmarkAudio);
+        $lastmark = new lastMark('uri','uri','time', 1234);
+        
+        $bookmarkSet = new bookmarkSet($title, 'uid', $lastmark);
+
+        $bookmarkObject = new bookmarkObject($bookmarkSet);
+        $bookmarkObject->lastModifiedDate = 1;
+        $this->assertFalse($bookmarkObject->validate());
+        //$this->assertContains('bookmarkObject.bookmarkSet', $bookmarkObject->getError());
+
+        $bookmarkObject = new bookmarkObject($bookmarkSet, 'nfd');
+        $this->assertFalse($bookmarkObject->validate());
+        //$this->assertContains('bookmarkObject.bookmarkSet', $bookmarkObject->getError());
+
+        $bookmarkObject = new bookmarkObject($bookmarkSet, '2016-03-11T14:23:23Z');
+        $this->assertTrue($bookmarkObject->validate());
+        //$this->assertContains('bookmarkObject.bookmarkSet', $bookmarkObject->getError());
+
+        $bookmarkObject = new bookmarkObject($bookmarkSet, '2016-03-11T14:23:23+00:00');
+        $this->assertTrue($bookmarkObject->validate());
+        //$this->assertContains('bookmarkObject.bookmarkSet', $bookmarkObject->getError());
+    }
+    
+}
+
+?>

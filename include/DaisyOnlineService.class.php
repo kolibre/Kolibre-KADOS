638e8e942ef70b961e798622fb526000ce17f3b0
Johan Abbors <jabbors@gmail.com>
2016-05-22 20:40:32 +0300

    Set version to 0.2

--- include/DaisyOnlineService.class.php
+++ include/DaisyOnlineService.class.php
@@ -59,7 +59,7 @@ require_once('setProgressStateResponse.class.php');
 
 class DaisyOnlineService
 {
-    const VERSION = '0.1';
+    const VERSION = '0.2';
 
     private $serviceAttributes = array();
     private $readingSystemAttributes = null;

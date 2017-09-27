6c3b64f53086fac42758558af41b7b3bdd348da7
Johan Abbors <jabbors@gmail.com>
2017-09-27 12:57:43 +0300

    Update adapter documentation

--- include/adapter/Adapter.class.php
+++ include/adapter/Adapter.class.php
@@ -813,19 +813,19 @@ abstract class Adapter
      * It is invoked by the service when getQuestions operation is called.
      * If the service supports DYNAMIC_MENUS, this method must be implemented.
      *
-     * @param array $response An associative array containing the reponses from a previous question.
+     * @param array $responses An associative array containing the reponses from a previous question.
      * <p>Example of an associative array</p>
      * <pre>
      * Array
      * (
      *     [0] => Array
      *     (
-     *         [questionId] => "question 1"
+     *         [questionID] => "question 1"
      *         [value] => "value is always a string"
      *     )
      *     [1] => Array
      *     (
-     *         [questionId] => "question 2"
+     *         [questionID] => "question 2"
      *         [base64] => "base64 encoded binary data"
      *     )
      * )
@@ -839,33 +839,44 @@ abstract class Adapter
      *     [0] => Array
      *     (
      *         [type] => "multipleChoiceQuestion"
-     *         [questionId] => "question 1"
+     *         [id] => "question 1"
      *         [choices] => Array
      *         (
      *             [0] => "choice 1"
      *             [1] => "choice 2"
      *             [2] => "choice 3"
      *         )
+     *         [allowMultipleSelections] = 1
      *     )
      *     [1] => Array
      *     (
-     *        [type] => "inputQuestions"
-     *        [questionId] => "question 2"
+     *        [type] => "inputQuestion"
+     *        [id] => "question 2"
+     *        [inputTypes] = Array
+     *        (
+     *            [0] => "TEXT_ALPHANUMERIC"
+     *        )
      *     )
      *     [2] => Array
      *     (
-     *        [type] => "inputQuestions"
-     *        [questionId] => "question 3"
+     *        [type] => "inputQuestion"
+     *        [id] => "question 3"
+     *        [inputTypes] = Array
+     *        (
+     *            [0] => "TEXT_ALPHANUMERIC"
+     *        )
+     *        [defaultValue] => "value"
      *     )
      *     [3] => Array
      *     (
      *         [type] => "multipleChoiceQuestion"
-     *         [questionId] => "question 4"
+     *         [id] => "question 4"
      *         [choices] => Array
      *         (
      *             [0] => "choice 1"
      *             [1] => "choice 2"
      *         )
+     *         [allowMultipleSelections] = 0
      *     )
      * )
      * </pre>
@@ -873,7 +884,7 @@ abstract class Adapter
      *
      * @throws AdapterException
      */
-    public function menuNext($response)
+    public function menuNext($responses)
     {
         return array();
     }
@@ -892,13 +903,14 @@ abstract class Adapter
      * Array
      * (
      *      [type] => "multipleChoiceQuestion"
-     *      [questionId] => "question 1"
+     *      [id] => "question 1"
      *      [choices] => Array
      *      (
      *          [0] => "choice 1"
      *          [1] => "choice 2"
      *          [2] => "choice 3"
      *      )
+     *      [allowMultipleSelections] = 0
      * )
      * </pre>
      *

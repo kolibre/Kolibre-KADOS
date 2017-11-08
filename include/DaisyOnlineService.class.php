1039b07dee7045e61e56eb89951227a0fb32e61b
Johan Abbors <jabbors@gmail.com>
2017-11-08 07:42:56 +0200

    Bumo version

--- include/DaisyOnlineService.class.php
+++ include/DaisyOnlineService.class.php
@@ -59,7 +59,7 @@ require_once('setProgressStateResponse.class.php');
 
 class DaisyOnlineService
 {
-    const VERSION = '0.2.2';
+    const VERSION = '0.2.3';
 
     private $optionalOperations = array();
     private $serviceAttributes = array();

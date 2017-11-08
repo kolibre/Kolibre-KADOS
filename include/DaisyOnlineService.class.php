3306f121bea4560d3ef77dd40aaf75df9dc28786
Johan Abbors <jabbors@gmail.com>
2017-11-08 07:43:44 +0200

    Bump version

--- include/DaisyOnlineService.class.php
+++ include/DaisyOnlineService.class.php
@@ -57,7 +57,7 @@ require_once('getKeyExchangeObjectResponse.class.php');
 
 class DaisyOnlineService
 {
-    const VERSION = '0.1.1';
+    const VERSION = '0.1.2';
 
     private $serviceAttributes = array();
     private $readingSystemAttributes = null;

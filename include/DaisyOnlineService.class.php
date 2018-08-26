eb50e60adcab8e154a0684a47a4a064eb6f84e14
Johan Abbors <jabbors@gmail.com>
2018-08-26 19:51:24 +0300

    Bump version to 0.2.4

--- include/DaisyOnlineService.class.php
+++ include/DaisyOnlineService.class.php
@@ -59,7 +59,7 @@ require_once('setProgressStateResponse.class.php');
 
 class DaisyOnlineService
 {
-    const VERSION = '0.2.3';
+    const VERSION = '0.2.4';
 
     private $optionalOperations = array();
     private $serviceAttributes = array();
